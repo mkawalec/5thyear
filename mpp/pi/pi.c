4dd040b4d574fb3e84762ea5c187ce12f0bc9d10
Michal Kawalec <michal@bazzle.me>
2013-09-25 15:13:19 +0100

    time measurement implemented

--- mpp/pi/pi.c
+++ mpp/pi/pi.c
@@ -19,31 +19,37 @@ main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
 
     double pi = 0;
-    int rank, total_processes;
+    double start_time = MPI_Wtime();
+    int rank, total_processes, j;
     int iterations = 840;
+    int repeats = 1000000;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &total_processes);
 
     if (rank > 0) {
-        int iter_per_rank = iterations/(total_processes - 1);
-        int start_iter = iter_per_rank * (rank - 1) + 1;
-        pi = compute_pi(start_iter, start_iter + iter_per_rank, iterations);
+        for (j = 0; j < repeats; ++j){
+            int iter_per_rank = iterations/(total_processes - 1);
+            int start_iter = iter_per_rank * (rank - 1) + 1;
+            pi = compute_pi(start_iter, start_iter + iter_per_rank, iterations);
 
-        MPI_Ssend(&pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Ssend(&pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        }
     }
     else {
-        double temp_pi;
-        size_t i = 0;
-        MPI_Status status;
+        for (j = 0; j < repeats; ++j){
+            double temp_pi;
+            size_t i = 0;
+            MPI_Status status;
 
-        for(i = 0; i < total_processes - 1; ++i) {
-            MPI_Recv(&temp_pi, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, 
-                    MPI_COMM_WORLD, &status);
-            pi += temp_pi;
+            for(i = 0; i < total_processes - 1; ++i) {
+                MPI_Recv(&temp_pi, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, 
+                        MPI_COMM_WORLD, &status);
+                pi += temp_pi;
+            }
+            pi /= iterations;
         }
-        pi /= iterations;
-        printf("PI: %lf\n", pi);
+        printf("Total time for %d repeats is %lf\n", repeats, MPI_Wtime() - start_time);
     }
 
 
