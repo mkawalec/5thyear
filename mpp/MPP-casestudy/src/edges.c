2c66746b3d2f94da340933f7ab092c74b73c9be2
Michal Kawalec <michal@bazzle.me>
2013-11-18 16:23:50 +0000

    added decomposition functions

--- mpp/MPP-casestudy/src/edges.c
+++ mpp/MPP-casestudy/src/edges.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <math.h>
+#include <limits.h>
 
 #include "pgmio.h"
 #include "arralloc.h"
@@ -44,6 +46,97 @@ void initialize_array(float **array, size_t dim_x, size_t dim_y)
     printf("done\n");
 }
 
+/*! Computes the total length of the circumference for
+ *  a decomposition of different sizes
+ */
+size_t circ(size_t dim_x, size_t dim_y, size_t part_x, size_t part_y)
+{
+    size_t length = 0, x_position, y_position;
+    for (x_position = 0; x_position < dim_x; x_position += part_x) {
+        for (y_position = 0; y_position < dim_y; y_position += part_y) {
+            if (y_position + part_y < dim_y) {
+                length += 2 * part_y;
+            } else {
+                length += 2 * (dim_y - y_position);
+            }
+
+            if (x_position + part_x < dim_x) {
+                length += 2 * part_x;
+            } else {
+                length += 2 * (dim_x - x_position);
+            }
+        }
+    }
+
+    return length;
+}
+
+/*! Returns the top upper corner and first bottom right coordinate 
+ * that is not in its domain (useful for < comparison).
+ */
+void my_pos(int rank, size_t dim_x, size_t dim_y, size_t part_x, size_t part_y,
+        size_t *start_x, size_t *start_y, size_t *end_x, size_t *end_y) {
+
+    size_t square_num = 0, x_position, y_position;
+
+    for (x_position = 0; x_position < dim_x; x_position += part_x) {
+        for (y_position = 0; y_position < dim_y; y_position += part_y) {
+            if (square_num == rank) {
+                *start_x = x_position;
+                *start_y = y_position;
+
+                if (y_position + part_y < dim_y) {
+                    *end_y = y_position + part_y;
+                } else {
+                    *end_y = dim_y - 1;
+                }
+                if (x_position + part_x < dim_x) {
+                    *end_x = x_position + part_x;
+                } else {
+                    *end_x = dim_x - 1;
+                }
+
+                printf("Hi from rank %d %ld %ld %ld %ld\n", square_num,
+                        *start_x, *start_y, *end_x, *end_y);
+                return;
+            }
+            square_num += 1;
+        }
+    }
+}
+
+
+void scatter(float *input, size_t dim_x, size_t dim_y, MPI_Comm communicator)
+{
+    // Number of parts
+    int comm_size, size_x, size_y, i, rank;
+    size_t circumference = ULONG_MAX;
+    MPI_Comm_size(communicator, &comm_size);
+    MPI_Comm_rank(communicator, &rank);
+
+    for (i = 1; i < sqrt(comm_size) + 1; ++i) {
+        if (comm_size%i == 0) {
+            int current_x, current_y;
+            size_t temp_circ;
+            current_x = ceil(dim_x / (double)i);
+            current_y = ceil(dim_y / (double) (comm_size / i));
+
+            if ((temp_circ = circ(dim_x, dim_y, current_x, current_y)) < circumference) {
+                circumference = temp_circ;
+                size_x = current_x;
+                size_y = current_y;
+            }
+        }
+    }
+
+    size_t start_x, start_y, end_x, end_y;
+    my_pos(rank, dim_x, dim_y, size_x, size_y,
+           &start_x, &start_y, &end_x, &end_y);
+
+    printf("%d %d\n", size_x, size_y);
+
+}
+
 int main(int argc, char *argv[])
 {
     /** The image filename is provided as the first
@@ -54,6 +147,11 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &process_count);
 
+    double t1 = MPI_Wtime();
+    scatter(NULL, 100, 100, MPI_COMM_WORLD);
+    printf("Time %lf\n", MPI_Wtime() - t1);
+    return 0;
+
     if (argc < 4) {
         if (rank == 0) 
             printf("This program is run as follows: %s image_name dim_x dim_y\n\n",
@@ -62,10 +160,9 @@ int main(int argc, char *argv[])
         return -1;
     }
     // Parsing the command line parameters
-    char **char_ptr = NULL;
     char *image_name = argv[1];
-    size_t dim_x = strtoul(argv[2], char_ptr, 10);
-    size_t dim_y = strtoul(argv[3], char_ptr, 10);
+    size_t dim_x = strtoul(argv[2], NULL, 10);
+    size_t dim_y = strtoul(argv[3], NULL, 10);
 
     MPI_Buffer_attach(malloc(BUFFSIZE), BUFFSIZE);
 
