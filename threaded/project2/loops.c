823309f200128d79e897cb721ca3c5ac2e5025aa
Michal Kawalec <michal@bazzle.me>
2013-11-23 20:29:35 +0000

    added comments and updated the results

--- threaded/project2/loops.c
+++ threaded/project2/loops.c
@@ -90,10 +90,31 @@ void init2(void){
 
 } 
 
+/**
+ *  @brief          a definition of a iteration range, 
+ *                  and a simple pair of integers
+ */ 
 struct Chunk {
     int start, end;
 };
 
+/**
+ *  @brief          Finds a an id of the most loaded thread
+ *  @param chunks   an array of unclaimed chunks
+ *  @param nthreads number of threads
+ *
+ *
+ *  This function does not perform any kind of locking
+ *  and thus answer it gives is not guaranteed to be 
+ *  'correct'. The only incorrectness that can occur
+ *  that would have a significant impact on both execution
+ *  time and the answer received occurs when a chunk
+ *  to be claimed was already claimed by another process
+ *  by the time this function completes.
+ *
+ *  This is an intended behaviour, and thus apply additional
+ *  checks to the numbers returned by this function.
+ */
 int get_most_loaded(struct Chunk *chunks, int nthreads)
 {
     int most_loaded = -1;
@@ -110,13 +131,14 @@ int get_most_loaded(struct Chunk *chunks, int nthreads)
     return most_loaded;
 }
 
-void runloop(int loopid)  {
+void runloop(int loopid)  
+{
     /* The total number of threads is acquired in this slightly odd 
      * way, as we want to allocate the later structures on the stack
      * and in a scope accessble to all threads
      */
     int nthreads; 
-#pragma omp parallel
+#pragma omp parallel default(none) shared(nthreads)
     {
 #pragma omp single 
         {
@@ -124,24 +146,30 @@ void runloop(int loopid)  {
         }
     }
             
-    /* Creating the 'ranges left to be computed'
-     * array, array of locks guarding access and 
-     * initializing the locks.
+    /* chunks contains still claimable ranges for each
+     * of the processes.
+     *
+     * chunk_locks is an array of locks. A lock
+     * with array index the same as a process number
+     * has to be set when a read or write, which results
+     * will be used for computation on a given chunk range,
+     * is attempted.
      */
     struct Chunk chunks[nthreads];
     omp_lock_t chunk_locks[nthreads];
+
     size_t i;
     for (i = 0; i < nthreads; ++i) omp_init_lock(&chunk_locks[i]);
 
 #pragma omp parallel default(none) shared(loopid, chunks, chunk_locks, nthreads) 
     {
-        int myid  = omp_get_thread_num();
-        int ipt = (int) ceil((double)N/(double)nthreads);
+        int myid    = omp_get_thread_num();
+        int ipt     = ceil(N / (double) nthreads);
 
         // Setting the upper and lower computation boundaries 
         // for this thread.
-        int lo = myid*ipt;
-        int hi = (myid+1)*ipt;
+        int lo = myid * ipt;
+        int hi = (myid + 1) * ipt;
         if (hi > N) hi = N; 
 
         /* The id of a process which chunk will be executed, if
@@ -161,9 +189,16 @@ void runloop(int loopid)  {
 
 #pragma omp barrier
         while (1) {
+            /* After the computation boundaries for the current iteration
+             * code below finished, start will contain the computation
+             * range start and end will contain computation range end.
+             */
             int start, end;
             omp_set_lock(&chunk_locks[myid]);
             if (chunks[myid].start < chunks[myid].end) {
+                /* If there is something to do in my own iterations 
+                 * range, claim it.
+                 */
                 start = chunks[myid].start;
                 end = start + (int)ceil((chunks[myid].end - chunks[myid].start)/
                         (double)nthreads);
@@ -171,6 +206,10 @@ void runloop(int loopid)  {
 
                 omp_unset_lock(&chunk_locks[myid]);
             } else if ((steal_from = get_most_loaded(chunks, nthreads)) != -1) {
+                /* Else, find the most loaded thread, 
+                 * steal a correct amount of iterations
+                 * and unset the locks
+                 */
                 omp_unset_lock(&chunk_locks[myid]);
                 omp_set_lock(&chunk_locks[steal_from]);
 
@@ -181,17 +220,20 @@ void runloop(int loopid)  {
 
                 omp_unset_lock(&chunk_locks[steal_from]);
             } else {
+                /* If there are no iterations left in all the
+                 * processes it means that the thread can finish
+                 */
                 omp_unset_lock(&chunk_locks[myid]);
                 break;
             }
 
-            /* There is a very small chance that in the 'else if' loop
-             * above a chunk to claim was the last one and it was claimed
-             * by another process. In such a case the condition checking would
-             * complete, but start would be set to end, as there are no iterations
-             * left to process in this process. There are most probably more 
-             * iterations in other threads, so the iteration-claiming process
-             * above should repeat.
+            /* There is a very small chance that in the 'else if' case
+             * above a process selectred to steal_from had all of its iterations
+             * claimed by the time the current process tries to claim its 
+             * iterations. In such a case the while loop would not break,
+             * but in the chunk range start would equal the end. As there may
+             * still be unclaimed iterations left in other threads, 
+             * the iteration-claiming process above should repeat.
              */
             if (start == end) 
                 continue;
