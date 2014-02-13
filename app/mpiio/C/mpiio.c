05b1ee4af854ed0fccfb7fd21164065bc559ab95
Michal Kawalec <michal@bazzle.me>
2014-02-13 22:59:27 +0000

    added a first draft

--- app/mpiio/C/mpiio.c
+++ app/mpiio/C/mpiio.c
@@ -19,7 +19,7 @@
 
 #define NDIM 2
 
-#define XPROCS 1
+#define XPROCS 4
 #define YPROCS 1
 
 #define NPROCS (XPROCS*YPROCS)
@@ -39,109 +39,110 @@
 
 void main(void)
 {
-  /*
-   *  pcoords stores the grid positions of each process
-   */
+    /*
+     *  pcoords stores the grid positions of each process
+     */
 
-  int pcoords[NPROCS][NDIM];
-  
-  /*
-   *  buf is the large buffer for the master to read into
-   *  x contains the local data only
-   */
+    int pcoords[NPROCS][NDIM];
 
-  float buf[NX][NY];
-  float x[NXP][NYP];
+    /*
+     *  buf is the large buffer for the master to read into
+     *  x contains the local data only
+     */
 
-  int rank, size;
-  int i, j;
+    float buf[NX][NY];
+    float x[NXP][NYP];
 
-  char filename[MAXFILENAME];
+    int rank, size;
+    int i, j;
 
-  MPI_Comm comm = MPI_COMM_WORLD;
+    char filename[MAXFILENAME];
 
-  MPI_Init(NULL, NULL);
+    MPI_Comm comm = MPI_COMM_WORLD;
 
-  MPI_Comm_size(comm, &size);
-  MPI_Comm_rank(comm, &rank);
+    MPI_Init(NULL, NULL);
 
-  /*
-   *  Check we are running on the correct number of processes
-   */
+    MPI_Comm_size(comm, &size);
+    MPI_Comm_rank(comm, &rank);
 
-  if (size != NPROCS)
+    /*
+     *  Check we are running on the correct number of processes
+     */
+
+    if (size != NPROCS)
     {
-      if (rank == 0)
-	{
-	  printf("ERROR: compiled for %d process(es), running on %d\n",
-		 NPROCS, size);
-	}
+        if (rank == 0)
+        {
+            printf("ERROR: compiled for %d process(es), running on %d\n",
+                    NPROCS, size);
+        }
 
-      MPI_Finalize();
-      exit(-1);
+        MPI_Finalize();
+        exit(-1);
     }
 
-  /*
-   *  Work out the coordinates of all the processes in the grid and
-   *  print them out
-   */
+    /*
+     *  Work out the coordinates of all the processes in the grid and
+     *  print them out
+     */
 
-  initpgrid(pcoords, XPROCS, YPROCS);
+    initpgrid(pcoords, XPROCS, YPROCS);
 
-  if (rank == 0)
+    if (rank == 0)
     {
-      printf("Running on %d process(es) in a %d x %d grid\n",
-	     NPROCS, XPROCS, YPROCS);
-      printf("\n");
+        printf("Running on %d process(es) in a %d x %d grid\n",
+                NPROCS, XPROCS, YPROCS);
+        printf("\n");
 
-      for (i=0; i < NPROCS; i++)
-	{
-	  printf("Process %2d has grid coordinates (%2d, %2d)\n",
-		 i, pcoords[i][0], pcoords[i][1]);
-	}
-      printf("\n");
+        for (i=0; i < NPROCS; i++)
+        {
+            printf("Process %2d has grid coordinates (%2d, %2d)\n",
+                    i, pcoords[i][0], pcoords[i][1]);
+        }
+        printf("\n");
     }
 
-  /*
-   *  Initialise the arrays to a grey value
-   */
+    /*
+     *  Initialise the arrays to a grey value
+     */
 
-  initarray(buf, NX,  NY );
-  initarray(x  , NXP, NYP);
+    initarray(buf, NX,  NY );
+    initarray(x  , NXP, NYP);
 
 
-  /*
-   *  Read the entire array on the master process
-   *  Passing "-1" as the rank argument means that the file name has no
-   *  trailing "_rank" appended to it, ie we read the global file
-   */
+    /*
+     *  Read the entire array on the master process
+     *  Passing "-1" as the rank argument means that the file name has no
+     *  trailing "_rank" appended to it, ie we read the global file
+     */
 
-  if (rank == 0)
+    MPI_File blah;
+    createfilename(filename, "cinput", NX, NY, -1);
+    //ioread (filename, buf, NX*NY);
+
+    MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &blah);
+    MPI_File_read_all(blah, buf, NY * NX / XPROCS, MPI_FLOAT, NULL);
+    MPI_File_close(&blah);
+
+    /*
+     *  Simply copy the data from buf to x
+     */
+
+    for (i=0; i < NXP; i++)
     {
-      createfilename(filename, "cinput", NX, NY, -1);
-      ioread (filename, buf, NX*NY);
-      printf("\n");
+        for (j=0; j < NYP; j++)
+        {
+            x[i][j] = buf[i][j];
+        }
     }
 
-  /*
-   *  Simply copy the data from buf to x
-   */
+    /*
+     *  Every process writes out its local data array x to an individually
+     *  named file which has the rank appended to the file name
+     */
 
-  for (i=0; i < NXP; i++)
-    {
-      for (j=0; j < NYP; j++)
-	{
-	  x[i][j] = buf[i][j];
-	}
-    }
+    createfilename(filename, "coutput", NXP, NYP, rank);
+    iowrite(filename, x, NXP*NYP);
 
-  /*
-   *  Every process writes out its local data array x to an individually
-   *  named file which has the rank appended to the file name
-   */
-
-  createfilename(filename, "coutput", NXP, NYP, rank);
-  iowrite(filename, x, NXP*NYP);
-
-  MPI_Finalize();
+    MPI_Finalize();
 }
